430e8d7580da4d1fad2a211a17c7cd51603d19a1
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2020-10-06 12:49:38 +0200

    f

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Verification",
-            targets: ["Verification"]),
+            targets: ["VerificationBin"]),
     ],
     dependencies: [
       .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
