93afc53c6869ee3ffe2583edafb7b3a1f9198c61
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2020-10-06 12:06:40 +0200

    ref

--- Package.swift
+++ Package.swift
@@ -18,10 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "Verification", path: "artifacts/Verification.xcframework", dependencies: [
-          .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
-          .package(url: "https://github.com/marmelroy/PhoneNumberKit", .upToNextMajor(from: "3.3.1"))
-        ])
+        .binaryTarget(name: "Verification", path: "artifacts/Verification.xcframework"),
         .testTarget(
             name: "VerificationTests",
             dependencies: ["Verification"]),
